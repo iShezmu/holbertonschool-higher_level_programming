8775eb24879acfdc5a0eac47e78c2db224f1a49c
Paulo Arteaga <7831@holbertonstudents.com>
2023-12-18 19:25:54 -0800

    test: implementing task 3

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+
+    if my_list is None:
+        return []
+    reversed_list = my_list[::-1]
+    for element in reversed_list:
+        print("{:d}".format(element))
