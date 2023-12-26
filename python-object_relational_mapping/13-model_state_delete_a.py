e1066020ca3d27b161311c2cbc58f9d69abf02a8
Paulo Arteaga <7831@holbertonstudents.com>
2023-12-25 21:04:06 -0800

    test: implementing task 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     from model_state import Base, State
     from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost/{}"""
                            .format(argv[1], argv[2], argv[3]))
 
     Base.metadata.create_all(engine)
