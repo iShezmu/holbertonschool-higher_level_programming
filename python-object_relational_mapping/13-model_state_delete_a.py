b5ee2f96d2f17e2156458f051767ba3799465271
Paulo Arteaga <7831@holbertonstudents.com>
2023-12-25 21:01:47 -0800

    test: implementing task 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     from model_state import Base, State
     from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine("""mysql+mysqldb://{}:{}@localhost/{}"""
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
                            .format(argv[1], argv[2], argv[3]))
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State)
-    states = states.filter(State.name.like('%a%')).order_by(State.id)
+    states = states.filter(State.name.like('a')).order_by(State.id)
     for state in states:
         session.delete(state)
     session.commit()
