d3ddb538ef5428e898910559b6f5de966913bc69
Paulo Arteaga <7831@holbertonstudents.com>
2023-12-26 10:21:37 -0800

    test: implementing task 0

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -16,19 +16,6 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    def __str__(self):
-        """Returns an informal and nicely printable string representation
-        of a Rectangle instance, filled with the '#' character"""
-        if self.__height == 0 or self.__width == 0:
-            return ''
-        return '\n'.join('#' * self.__width for _ in range(self.__height))
-
-    def __repr__(self):
-        """Return a string representation of a Rectangle 
-        that is able to recreate a new instance by using eval()
-        """
-        return "Rectangle({}, {})".format(self.__width, self.__height)
-
     @property
     def width(self):
         """Retrieves the width of a Rectangle instance."""
@@ -82,3 +69,16 @@ class Rectangle:
         else:
             perimeter = 2 * (self.__width + self.__height)
         return perimeter
+
+    def __str__(self):
+        """Returns an informal and nicely printable string representation
+        of a Rectangle instance, filled with the '#' character"""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        return '\n'.join('#' * self.__width for _ in range(self.__height))
+
+    def __repr__(self):
+        """Return a string representation of a Rectangle
+        that is able to recreate a new instance by using eval()
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
