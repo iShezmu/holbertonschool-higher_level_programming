ba1c151cbcbaa557d7f5bf5d1b5d94e42e061221
Paulo Arteaga <7831@holbertonstudents.com>
2023-12-27 10:02:28 -0800

    test: implementing task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -24,4 +24,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return a string representation of the instance"""
-        return f"[Square] {self.__size}/{self-size}"
+        return f"[Square] {self.__size}/{self.__size}"
