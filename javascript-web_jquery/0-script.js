64a154eea30cf3701efdbd2cb77dc7f39aa75aba
Paulo Arteaga <142102456+iShezmu@users.noreply.github.com>
2024-04-06 19:50:05 -0500

    Delete javascript-web_jquery directory

--- javascript-web_jquery/0-script.js
+++
@@ -1,5 +0,0 @@
-// JavaScript script that updates text color
-document.addEventListener('DOMContentLoaded', () => {
-	  const headerElement = document.querySelector('header')
-	  headerElement.style.color = '#FF0000'
-})
--- javascript-web_jquery/1-script.js
+++
@@ -1,8 +0,0 @@
-// Script that updates the text color
-$(document).ready(() => {
-  // Find the <header> element
-  const headerElement = $('header');
-
-  // Set the text color to red (#FF0000)
-  headerElement.css('color', '#FF0000');
-});
